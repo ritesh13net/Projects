1152ffc3df0fd1da2129cd30681bebce4655aa28
Ritesh Kumar <ritesh13net@gmail.com>
2025-10-22 18:36:18 +0530

    Add files via upload

---
+++ Flask/jinja.py
@@ -0,0 +1,58 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+## Variable Rule
+@app.route('/success/<int:score>')
+def success(score):
+    res=""
+    if score>=50:
+        res="PASSED"
+    else:
+        res="FAILED"
+
+    return render_template('result.html',results=res)
+
+## Variable Rule
+@app.route('/successres/<int:score>')
+def successres(score):
+    res=""
+    if score>=50:
+        res="PASSED"
+    else:
+        res="FAILED"
+    
+    exp={'score':score,"res":res}
+
+    return render_template('result1.html',results=exp)
+
+## if confition
+@app.route('/sucessif/<int:score>')
+def successif(score):
+
+    return render_template('result.html',results=score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html',results=score)
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres',score=total_score))
+            
+        
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
